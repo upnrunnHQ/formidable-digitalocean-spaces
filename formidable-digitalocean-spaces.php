d510e5fde1800329675412a816f04a827b43551a
Bhoot Biswas <bhoot.biswas@gmail.com>
2022-02-23 11:15:07 +0600

    Add initial files

---
+++ formidable-digitalocean-spaces.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Formidable Digitalocean Spaces
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     formidable-digitalocean-spaces
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Formidable_Digitalocean_Spaces
+ */
+
+// Your code starts here.
